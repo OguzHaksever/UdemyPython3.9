77cd3cb7e7cb8beacd3411b265e2208eb2a3fe90
OguzHaksever <oguzhaksever1@gmail.com>
2020-10-18 17:28:53 +0300

    first change

--- conditions.py
+++ conditions.py
@@ -1,7 +1,7 @@
 age = int(input("How old are you? "))
 
 if 16 <= age <= 65:                     # print if i in range (16, 66) aynı sonucu çıkarır
-    print("Have a good day at work.")
+    print("Have a good  at work.")
 else:
     print("You shouldn't be working.")
 
