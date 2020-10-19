2ccb86f88eb40bcaf6bb00a4c48368cdedb34c28
OguzHaksever <oguzhaksever1@gmail.com>
2020-10-19 14:52:59 +0300

    Added 2 new lesson codes

--- tuples_intro.py
+++ tuples_intro.py
@@ -1,10 +1,12 @@
-welcome = "Welcome to my Nightmare", "Alice Cooper", 1975
-bad = "Bad Company", "Budgie", 1974
-budgie = "Nightflight", "Budgie", 1981
-imelda = "More Mayhem", "Emilda May", 2011
-metallica = "Ride the Lightning", "Metallica", 1984
+albums = [("Welcome to my Nightmare", "Alice Cooper", 1975),
+          ("Bad Company", "Budgie", 1974),
+          ("Nightflight", "Budgie", 1981),
+          ("More Mayhem", "Emilda May", 2011),
+          ("Ride the Lightning", "Metallica", 1984),
+          ]
 
-print(metallica)
+print(len(albums))
 
-metallica2 = list(metallica)
-print(metallica2)
+for name, artist, year in albums:
+    print("Album: {}, Artist: {}, Year: {}".format(name, artist, year))
+
